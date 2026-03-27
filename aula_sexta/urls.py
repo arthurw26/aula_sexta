1dfb641d6cd47a95292ae0845ee891652ae9f945
arthurw26 <arthur.wiatrowski@gmail.com>
2026-03-27 20:07:47 -0300

    create recipes

---
+++ aula_sexta/urls.py
@@ -0,0 +1,38 @@
+"""
+URL configuration for aula_sexta project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+from recipes.views import _home
+
+
+# HTTP REQUEST <- RESPONSE
+#def home(request):
+#   return HttpResponse('home')
+
+def sobre(request):
+    return HttpResponse('sobre')
+
+def contato(request):
+    return HttpResponse('contato')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', _home),#home
+    path('sobre/', sobre),#sobre
+    path('contato/', contato),#contato
+]
